bc4a8529fc960b790f51adf2f96ea72c8fcf01f8
Francisco Soto <panchon.ues@gmail.com>
2024-06-06 22:47:50 -0700

    project structure and folders

---
+++ src/presentation/screens/details/DetailsScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+
+
+const DetailsScreen = () => {
+  return (
+    <View>
+      <Text>DetailsScreen</Text>
+    </View>
+  );
+}
+
+export default DetailsScreen;
