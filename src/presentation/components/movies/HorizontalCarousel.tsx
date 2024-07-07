84decfaf61ec20ac0428c70125b6dd36d905a142
Francisco Soto <panchon.ues@gmail.com>
2024-07-07 14:21:43 -0700

    main carousel

---
+++ src/presentation/components/movies/HorizontalCarousel.tsx
@@ -0,0 +1,12 @@
+import { Text, StyleSheet, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class HorizontalCarousel extends Component {
+  render() {
+    return (
+      <View>
+        <Text>HorizontalCarousel</Text>
+      </View>
+    )
+  }
+}
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,15 +1,34 @@
 import { View, Text } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+import { ScrollView } from 'react-native-gesture-handler';
+
 import { useMovies } from '../../hooks/useMovies';
+import PosterCarousel from '../../components/movies/PosterCarousel';
+import HorizontalCarousel from '../../components/movies/HorizontalCarousel';
 
 
 const HomeScreen = () => {
 
-  const {} = useMovies();
+  const {top}=useSafeAreaInsets();
+
+  const {isLoading, nowPlaying} = useMovies();
+
+  if(isLoading) {
+    return (<Text>Cargando...</Text>)
+  }
 
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <ScrollView style={{marginTop: top+20, paddingBottom: 30}}>
+      <View>
+        {/* principal */}
+        <PosterCarousel movies={nowPlaying} />
+
+        {/* pupular movies */}
+        <HorizontalCarousel />
+
+      </View>
+    </ScrollView>
   );
 }
 
