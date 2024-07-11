46fee1d08e5fcbc4ad6d8929bd15940ec713087e
Francisco Soto <panchon.ues@gmail.com>
2024-07-11 16:41:51 -0700

    env types

--- src/config/adapters/movieDB.adapter.ts
+++ src/config/adapters/movieDB.adapter.ts
@@ -1,3 +1,4 @@
+import { THE_MOVIE_DB_KEY } from "@env";
 import { AxiosAdapter } from "./http/axios.adapter";
 
 
@@ -5,7 +6,8 @@ import { AxiosAdapter } from "./http/axios.adapter";
 export const movieDBfetcher = new AxiosAdapter({
     baseUrl: 'https://api.themoviedb.org/3/movie',
     params: {
-        api_key: '2694d5fcbacac44f24e5f99fd5960389',
+        // api_key: '2694d5fcbacac44f24e5f99fd5960389',
+        api_key: THE_MOVIE_DB_KEY,
         language: 'es'
     }
 });
